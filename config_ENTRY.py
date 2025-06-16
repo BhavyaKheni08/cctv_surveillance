47da6d76d37183bd1934daad03c848507720f961
BhavyaKheni08 <khenibhavya@gmail.com>
2025-06-16 08:43:51 +0530

    Add files via upload

---
+++ config_ENTRY.py
@@ -0,0 +1,5 @@
+DEFAULT_VIDEO_SOURCE = "/Users/bhavyakheni/Desktop/Clip-2.mp4"
+
+ENTRY_ZONES = [
+    [(963, 215), (967, 357), (1265, 366), (1219, 231)],
+]
---
+++ config_STOPPING.py
@@ -0,0 +1,43 @@
+import os
+
+YOLO_GENERAL_MODEL_PATH = "/Users/bhavyakheni/Desktop/cctv_surveillance/models/yolo11n.pt"
+YOLO_POSE_MODEL_PATH = "/Users/bhavyakheni/Desktop/cctv_surveillance/models/yolo11s-pose.pt"
+
+DEFAULT_VIDEO_SOURCE = "/Users/bhavyakheni/Desktop/Clip-1.mp4"
+
+CLASS_NAMES = {
+    0: 'person',
+    1: 'bicycle',
+    2: 'car',
+    3: 'motorcycle',
+    5: 'bus',
+    7: 'truck',
+}
+
+PERSON_CLASS_ID = 0
+VEHICLE_CLASS_IDS = [2, 5, 7]
+BIKE_CLASS_IDS = [1, 3]
+
+FORBIDDEN_ZONE_COOLDOWN_SECONDS = 5
+VIOLATION_IMAGE_SAVE_DIR = "violations"
+if not os.path.exists(VIOLATION_IMAGE_SAVE_DIR):
+    os.makedirs(VIOLATION_IMAGE_SAVE_DIR)
+
+VANDALISM_DIST_THRESH = 100
+VANDALISM_MOTION_THRESH = 1500
+VANDALISM_TRACKER_MAX_AGE = 30
+
+STOP_DURATION_SECONDS = 3
+PERSON_PROXIMITY_THRESHOLD_PIXELS = 100
+PERSON_GRACE_PERIOD_SECONDS = 3.0
+
+DISPLAY_WINDOW_NAME_PREFIX = "Surveillance Feed"
+ANNOTATION_COLOR_VIOLATION = (0, 0, 255)
+ANNOTATION_COLOR_WARNING = (0, 165, 255)
+ANNOTATION_COLOR_NORMAL = (0, 255, 0)
+ANNOTATION_COLOR_ZONE_BOUNDARY = (255, 255, 0)
+ANNOTATION_COLOR_ZONE_FILL = (255, 0, 0)
+
+NO_STOPPING_ZONES = [
+    [[1170, 658], [1376, 467], [1528, 475], [1452, 691], [1171, 658]],
+]
